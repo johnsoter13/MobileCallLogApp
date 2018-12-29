1259c27962176441328a88595bf52e2968dc68d8
John Soter <john.soter@outlook.com>
2018-12-28 18:01:36 -0800

    can handle permissions now

--- app/src/main/java/edu/uw/jsoter/mobilecalllog/MainActivity.kt
+++ app/src/main/java/edu/uw/jsoter/mobilecalllog/MainActivity.kt
@@ -1,10 +1,16 @@
 package edu.uw.jsoter.mobilecalllog
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private var Phone_Call_REQUEST_CODE = 1
     private lateinit var mAdapter: PhoneCallRecyclerAdapter
     private var phoneCalls = listOf<PhoneCall>()
 
@@ -12,7 +18,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            getPhoneCalls()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CALL_LOG), Phone_Call_REQUEST_CODE)
+        }
     }
 
+    private fun getPhoneCalls() {
+        
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            Phone_Call_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
 }
